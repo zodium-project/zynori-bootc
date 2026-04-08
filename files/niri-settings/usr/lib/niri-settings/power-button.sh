3d0d58ffa9ac930935bc79f9aa65ed37e0ccbdaf
zodium#dev <dev.zodium.project@gmail.com>
2026-04-08 12:15:32 +0530

    (Add) niri dots

---
+++ files/niri-settings/usr/lib/niri-settings/power-button.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+LOCK="/tmp/noctalia-sessionmenu-power.lock"
+NOW=$(date +%s%3N) # milliseconds
+
+if [[ -f "$LOCK" ]]; then
+    LAST=$(cat "$LOCK" 2>/dev/null || echo 0)
+else
+    LAST=0
+fi
+
+# Ignore if triggered again within 300ms
+if (( NOW - LAST < 300 )); then
+    exit 0
+fi
+
+echo "$NOW" > "$LOCK"
+qs -c noctalia-shell ipc call sessionMenu toggle
